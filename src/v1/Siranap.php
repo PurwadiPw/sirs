9c507fb5d7ee241d22359f9b953c4c461fe5027f
Purwadi <purwadie97@gmail.com>
2019-05-20 11:12:13 +0700

    update

--- src/v1/Siranap.php
+++ src/v1/Siranap.php
@@ -17,4 +17,33 @@ class Siranap extends SirsIntegration
         $response = $this->post($service, $data);
         return json_decode($response, true);
     }
+
+    public function kunjungan($pelayanan, $tgl, $data)
+    {
+        /** 
+         * $pelayanan = irj, iri, igd
+         */
+        $response = $this->post($pelayanan.'/'.$tgl, $data);
+        return json_decode($response, true);
+    }
+
+    public function big10Penyakit($pelayanan, $bulan, $tahun, $data)
+    {
+        /** 
+         * $pelayanan = irj, iri
+         * $bulan = 1-12
+         */
+        $response = $this->post('diagnosa_'.$pelayanan.'/'.$bulan.'-'.$tahun, $data);
+        return json_decode($response, true);
+    }
+
+    public function indikatorPelayanan($indikator, $bulan, $tahun, $data)
+    {
+        /** 
+         * $indikator = bor
+         * $bulan = 1-12
+         */
+        $response = $this->post($indikator.'/'.$bulan.'-'.$tahun, $data);
+        return json_decode($response, true);
+    }
 }
